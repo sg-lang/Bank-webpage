33e966a95b617885c38e9c0f696c4d19d8893990
sg-lang <148348994+sg-lang@users.noreply.github.com>
2025-04-04 18:29:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def account_details(account_number):
     }), 200
 
 if __name__ == '__main__':
-import os
+    import os
 
 port = os.environ.get('PORT', 5000)  # Get the port from environment or default to 5000
 app.run(host='0.0.0.0', port=port, debug=True)
