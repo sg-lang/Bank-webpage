36468c5758c61686abcc2404314cff260bef8d92
sg-lang <148348994+sg-lang@users.noreply.github.com>
2025-04-04 18:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,16 @@
 
 # In[ ]:
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Bank Webpage!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 from flask import Flask, request, jsonify
 
